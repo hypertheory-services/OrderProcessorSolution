6e2fe5f8c08c9415bf23db7aaf82cb821f653b19
Jeffry Gonzalez <jeff@hypertheory.com>
2022-06-22 13:22:22 -0400

    ready

---
+++ OrderProcessorApi.IntegrationTests/PlacingAnOrderNeedsAuthentication.cs
@@ -0,0 +1,28 @@
+﻿
+using OrderProcessorApi.Controllers;
+
+namespace OrderProcessorApi.IntegrationTests;
+
+public class PlacingAnOrderNeedsAuthentication : IClassFixture<FixtureBase>
+{
+
+    private readonly IAlbaHost _host;
+
+    public PlacingAnOrderNeedsAuthentication(FixtureBase fixture)
+    {
+        _host = fixture.AlbaHost;
+    }
+
+    [Fact]
+    public async Task RequiresAuthentication()
+    {
+        var response = await _host.Scenario(api =>
+        {
+            var cart = new ShoppingCart(new string[] { "hat", "coat" }, "Expedite");
+
+            api.Post.Json(cart).ToUrl("/my/orders");
+            api.StatusCodeShouldBe(401);
+        });
+    }
+}
+
